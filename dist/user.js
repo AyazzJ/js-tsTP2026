039ad70d33129d72d6d847545a2115f484a5745c
Ayaz JUBAER <167209102+AyazzJ@users.noreply.github.com>
2026-03-13 14:48:35 +0100

    Step 1

---
+++ dist/user.js
@@ -0,0 +1,9 @@
+// Classe User
+export class User {
+    constructor(id, name, wallet) {
+        this.id = id;
+        this.name = name;
+        this.wallet = wallet;
+        this.orders = [];
+    }
+}
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+import { Meal } from "./meals.js";
+import { User, Order } from "./user.js";
+
+console.log("App loaded");
